08afc863514a5593fcaccf6f383828c77fc84eb3
Flavio Silva <fal_silva@hotmail.com>
2024-11-21 11:02:53 -0300

    montagem da função: mostrarQuestoes()

--- script.js
+++ script.js
@@ -9,7 +9,19 @@ mostrarQuestoes()
 function mostrarQuestoes(){
     if(questions[questaoAtual]){ //acessando o arquivo questions.js para varrer o array e retornar as questoes
         let q = questions[questaoAtual]
-        console.log(q.question)
+        
+        document.querySelector('.scoreArea').style.display = 'none'
+        document.querySelector('.questionArea').style.display = 'block'
+
+        document.querySelector('.question').innerHTML = q.question
+        //Mostrando as alternativas na tela:
+        let opcaoHtml = ''
+        for( let i in q.options){
+            opcaoHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span> ${q.options[i]}</div>`
+        }
+        document.querySelector('.options').innerHTML = opcaoHtml
+
+
     } else {
         //acaba as questões
     }
