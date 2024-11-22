32a241ec5cb24a29778bec63c9f7c47a46f62850
Flavio Silva <fal_silva@hotmail.com>
2024-11-22 16:17:17 -0300

    condicional if para verificar seu nome digitado é igual a vazio

--- inicio.js
+++ inicio.js
@@ -8,9 +8,17 @@ botao.addEventListener('click', function() {
     let nomeDigitado = texto.value
     console.log(nomeDigitado)
 
-    window.location.href = 'index.html'
-    localStorage.setItem('nome', nomeDigitado)
-    
+    if(nomeDigitado === ''){
+        alert('Digite seu nome por favor!')
+        window.location.href = 'inicio.html'
+    } else {
+
+        window.location.href = 'index.html'
+        localStorage.setItem('nome', nomeDigitado)
+    }
+
+
+
     // document.querySelector('.outro_texto').innerHTML = nomeDigitado
 })
 
